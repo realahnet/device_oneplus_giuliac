6c57d53e8c589de8660df72239e2ee8a338180ad
realahnet <syedmusicrealise@gmail.com>
2026-02-08 03:53:18 +0500

    giuliac: Sync Camera blob list with LOS
    * Move them back to proprietary-files
    * Keep CameraHWConfiguration.config and keep the following changes:
    
    - Disable face detection AE
    - Enable AUX cameras for 3rd party apps

--- device.mk
+++ device.mk
@@ -26,12 +26,6 @@ TARGET_SCREEN_WIDTH := 1264
 
 # Camera
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/camera/self_ois.ocfg:$(TARGET_COPY_OUT_ODM)/etc/camera/self_ois.ocfg \
-    $(LOCAL_PATH)/configs/camera/config/imx581:$(TARGET_COPY_OUT_ODM)/etc/camera/config/imx581 \
-    $(LOCAL_PATH)/configs/camera/config/lyt808:$(TARGET_COPY_OUT_ODM)/etc/camera/config/lyt808 \
-    $(LOCAL_PATH)/configs/camera/config/oplus_camera_algo_switch_config:$(TARGET_COPY_OUT_ODM)/etc/camera/config/oplus_camera_algo_switch_config \
-    $(LOCAL_PATH)/configs/camera/config/oplus_camera_aps_config:$(TARGET_COPY_OUT_ODM)/etc/camera/config/oplus_camera_aps_config \
-    $(LOCAL_PATH)/configs/camera/config/ov64b40_tele:$(TARGET_COPY_OUT_ODM)/etc/camera/config/ov64b40_tele \
     $(LOCAL_PATH)/configs/camera/CameraHWConfiguration.config:$(TARGET_COPY_OUT_ODM)/etc/camera/CameraHWConfiguration.config
 
 # Display
