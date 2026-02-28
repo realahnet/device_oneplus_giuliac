77da67bf943d9daffafdd4e45c48308505fac34b
realahnet <syedmusicrealise@gmail.com>
2026-02-28 20:42:22 +0500

    Revert "giuliac: [TEMP]: Remove pixelworks impl"
    This reverts commit 5669c10f0f08c7a2b5a10c91206709b8ac2167da.

--- device.mk
+++ device.mk
@@ -46,6 +46,7 @@ PRODUCT_COPY_FILES += \
 $(call soong_config_set,qtidisplay,bitclk_rate_permissive,true)
 $(call soong_config_set,surfaceflinger,frame_rate_category_high,120)
 $(call soong_config_set,surfaceflinger,frame_rate_category_min,1)
+$(call soong_config_set,qtidisplay,sm8650_pxlw_vendor_namespace,vendor/oneplus/giuliac)
 
 # DisplayConfig
 PRODUCT_COPY_FILES += \
