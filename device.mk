feb7acb3a299077f8036bbcf4c69fe566d90c65f
realahnet <syedmusicrealise@gmail.com>
2026-06-14 00:37:14 +0500

    Revert "giuliac: camera: Import a few configs into tree"
    This reverts commit 33ddee5f9c3a3220e0c6c22ec397503612fb3053.

--- device.mk
+++ device.mk
@@ -25,10 +25,6 @@ TARGET_SCREEN_WIDTH := 1264
 # Camera
 $(call soong_config_set,libbinder,binder_vm_size_4mb,true)
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/camera/config/imx355:$(TARGET_COPY_OUT_ODM)/etc/camera/config/imx355 \
-    $(LOCAL_PATH)/configs/camera/config/imx906:$(TARGET_COPY_OUT_ODM)/etc/camera/config/imx906 \
-    $(LOCAL_PATH)/configs/camera/config/oplus_camera_algo_switch_config:$(TARGET_COPY_OUT_ODM)/etc/camera/config/oplus_camera_algo_switch_config \
-    $(LOCAL_PATH)/configs/camera/config/oplus_camera_aps_config:$(TARGET_COPY_OUT_ODM)/etc/camera/config/oplus_camera_aps_config \
     $(LOCAL_PATH)/configs/camera/CameraHWConfiguration.config:$(TARGET_COPY_OUT_ODM)/etc/camera/CameraHWConfiguration.config
 
 # Display
