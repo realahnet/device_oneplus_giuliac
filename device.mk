b510e6a6699dff8761f9468da1bce50e32f74784
John Galt <johngaltfirstrun@gmail.com>
2026-06-16 11:37:32 -0400

    giuliac: set binder vm size to 4M
    Solves significant cam spam and errors/retries.

--- device.mk
+++ device.mk
@@ -23,6 +23,7 @@ TARGET_SCREEN_HEIGHT := 2780
 TARGET_SCREEN_WIDTH := 1264
 
 # Camera
+$(call soong_config_set,libbinder,binder_vm_size_4mb,true)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/camera/config/imx355:$(TARGET_COPY_OUT_ODM)/etc/camera/config/imx355 \
     $(LOCAL_PATH)/configs/camera/config/imx906:$(TARGET_COPY_OUT_ODM)/etc/camera/config/imx906 \
